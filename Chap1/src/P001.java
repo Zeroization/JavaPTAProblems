ec4d42b1ef2b4a8880873aba24e3bc0311b71ea4
1255108389 <1255108389@qq.com>
2022-08-26 11:32:58 +0800

    add CHap1_P001

--- Chap1/src/P001.java
+++ Chap1/src/P001.java
@@ -1,5 +1,16 @@
 //第一个PTA上Java程序 https://pintia.cn/problem-sets/1542514312114589696/problems/1542514527521468416
+import java.util.Scanner;
 public class P001 {
-    //test
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        while(scan.hasNext()){
+            int a = scan.nextInt();
+            int b = scan.nextInt();
+            if (Math.abs(a) > 1000)
+                System.out.println("|a|>1000");
+            else
+                System.out.println(a + b);
+        }
+    }
 
 }
